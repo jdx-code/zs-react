f6db60691a420023e29ed579d743af96f625c77d
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-11-18 14:00:46 +0530

    Navbar responsiveness completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,37 @@
-import React from "react"
+import React, { useState } from "react";
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 
 const Navbar = () =>{
+
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
     return(
         <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-            <h1 className="w-full text-green-100 text-lg font-bold uppercase mt-2 ml-2">Zephyr Solutions</h1>
-            <ul className="flex">
+            <h1 className="w-full text-green-100 text-lg font-bold uppercase m-4">Zephyr Solutions</h1>
+            <ul className="hidden md:flex">
                 <li className="p-4">Home</li>
                 <li className="p-4">About</li>
                 <li className="p-4">Services</li>
                 <li className="p-4">Career</li>
                 <li className="p-4">Contact</li>
             </ul>
+            <div onClick={handleNav} className="block md:hidden">
+                {!nav ? <AiOutlineClose size={20}/>: <AiOutlineMenu size={20}/>}                
+            </div>
+            <div className={!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
+            <h1 className="w-full text-green-100 text-lg font-bold uppercase m-4">Zephyr Solutions</h1>
+                <ul className="uppercase p-4">
+                    <li className="p-4 border-b border-gray-600">Home</li>
+                    <li className="p-4 border-b border-gray-600">About</li>
+                    <li className="p-4 border-b border-gray-600">Services</li>
+                    <li className="p-4 border-b border-gray-600">Career</li>
+                    <li className="p-4">Contact</li>
+                </ul>
+            </div>
         </div>
     )
 }
