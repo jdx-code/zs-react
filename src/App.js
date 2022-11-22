89d295f03d9640099388cb521e87a2e6f6523a07
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-11-22 16:22:36 +0530

    pushing Client and techStack components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import ChooseUs from "./components/ChooseUs";
 import Offers from "./components/Offers";
+import Techstacks from "./components/Techstacks";
+import Clients from "./components/Clients";
 function App() {
   return (
     <div>
       <Navbar/>  
       <Hero/>
       <ChooseUs/>
+      <Clients/>      
       <Offers/>
+      <Techstacks/>
     </div>
   );
 }
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import IndiaPost from '../assets/indiapost.png';
+import Nhidcl from '../assets/nhidcl.jpg';
 
 const Clients = () => {
     return(
-        <div className="w-full">
-
+        <div className="w-full bg-white border-2 border-green-500 py-8">
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="py-2 mb-12 text-2xl font-bold md:text-3xl">Some of our clients</h1>
+                <div className="flex flex-wrap justify-center">
+                    <img className="w-[150px] h-[90px] px-6" src={IndiaPost}/>                
+                    <img className="w-[150px] h-[90px] px-6" src={Nhidcl}/>                
+                    <img className="w-[150px] h-[90px] px-6" src={IndiaPost}/>                
+                    <img className="w-[150px] h-[90px] px-6" src={Nhidcl}/>
+                    <img className="w-[150px] h-[90px] px-6" src={IndiaPost}/>                
+                    <img className="w-[150px] h-[90px] px-6" src={Nhidcl}/>                    
+                </div>            
+            </div>            
         </div>
     )
 }
