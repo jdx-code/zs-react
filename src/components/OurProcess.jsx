482504a8968f5fb21aadcb3e961b62f63df27561
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-11-24 23:29:31 +0530

    Cards fixed

--- src/components/OurProcess.jsx
+++ src/components/OurProcess.jsx
@@ -6,26 +6,26 @@ import LaunchSupport from '../assets/support-svgrepo-com.svg';
 
 const OurProcess = () => {
     return(
-        <div className="w-full bg-white text-black border-2 border-blue-500 py-24">
-            <div className='flex flex-col justify-center items-center px-24'>
+        <div className="w-full bg-white text-black border-2 border-blue-500 py-12">
+            <div className='flex flex-col justify-center items-center px-24 text-gray-800'>
                 <div className='mx-16 px-24 text-center'>
-                    <h1 className='text-2xl font-bold'>We take full-cycle care of our clients</h1>
+                    <h1 className='text-2xl m-8 text-center font-bold md:text-3xl'>We take full-cycle care of our clients</h1>
                     <p>Clarity, transparency, and integrity are among the core values of our team at Catware. As such, we don’t just “work on projects”. We bring our experience and expertise to every stage of the development lifecycle. You will have a reliable IT partner with Catware.</p>
                 </div>                
-                <div className='grid md:grid-cols-4 w-full gap-8'>
-                    <div className='w-full bg-green-100 p-8 text-center rounded-xl shadow-2xl'>
+                <div className='grid md:grid-cols-4 w-full gap-8 mt-8 mb-8'>
+                    <div className='w-full bg-pink-200 p-8 text-center rounded-xl shadow-2xl'>
                         <img className='w-24 mx-auto' src={FirstInteraction}/>
                         <p>First Interaction</p>
                     </div>                                       
-                    <div className='w-full bg-green-100 p-8 text-center rounded-xl shadow-2xl'>
+                    <div className='w-full bg-pink-200 p-8 text-center rounded-xl shadow-2xl'>
                         <img className='w-24 mx-auto' src={EvaluationScope}/>
                         <p>Evaluation and scope</p>
                     </div>
-                    <div className='w-full bg-green-100 p-8 text-center rounded-xl shadow-2xl'>
+                    <div className='w-full bg-pink-200 p-8 text-center rounded-xl shadow-2xl'>
                         <img className='w-24 mx-auto' src={DevTesting}/>
                         <p>Development and testing</p>
                     </div>
-                    <div className='w-full bg-green-100 p-8 text-center rounded-xl shadow-2xl'>
+                    <div className='w-full bg-pink-200 p-8 text-center rounded-xl shadow-2xl'>
                         <img className='w-24 mx-auto' src={LaunchSupport}/>
                         <p>Post-launch support</p>
                     </div>
