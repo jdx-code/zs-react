c2d6e38a0bc598aeb4d65fb5e5fd4184eeed7260
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-11-24 23:12:08 +0530

    Added Cards in OurProcess component

---
+++ src/components/BackOfCard.js
@@ -0,0 +1,12 @@
+function BackOfCard() {
+    return (
+      <div className="absolute inset-0 w-full h-full flex justify-center items-center bg-green-100 text-gray-800 transition-all z-10 card-back">
+        <p>
+        You share your vision. All mission-sensitive information is protected by an NDA.
+We assign a dedicated Business Development Manager to your project.
+        </p>
+      </div>
+    );
+  }
+
+export default BackOfCard;
--- src/components/OurProcess.jsx
+++ src/components/OurProcess.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import EvaluationScope from '../assets/graph-svgrepo-com.svg';
+import DevTesting from '../assets/code-svgrepo-com.svg';
+import LaunchSupport from '../assets/support-svgrepo-com.svg';
+import FrontOfCard from './FrontOfCard';
+import BackOfCard from './BackOfCard';
+
 const OurProcess = () => {
     return(
         <div className="w-full bg-white text-black border-2 border-blue-500 py-24">
@@ -8,22 +14,24 @@ const OurProcess = () => {
                     <h1 className='text-2xl font-bold'>We take full-cycle care of our clients</h1>
                     <p>Clarity, transparency, and integrity are among the core values of our team at Catware. As such, we don’t just “work on projects”. We bring our experience and expertise to every stage of the development lifecycle. You will have a reliable IT partner with Catware.</p>
                 </div>                
-                <div className='grid md:grid-cols-4 w-full'>
-                    <div className=''>
-                        <p>Icon</p>
-                        <p>Lorem ipsum dolor sit amet.</p>
+                <div className='grid md:grid-cols-4 w-full gap-8'>
+                    <div className='relative w-full rounded-2xl shadow-2xl text-white overflow-hidden cursor-pointer transition-all duration-1000 card'>
+                        <FrontOfCard/>
+                        <BackOfCard/>
                     </div>
-                    <div className=''>
-                        <p>Icon</p>
-                        <p>Lorem ipsum dolor sit amet.</p>
+                    
+                    
+                    <div className='w-full bg-green-100 p-8 text-center rounded-xl shadow-2xl'>
+                        <img className='w-24 mx-auto' src={EvaluationScope}/>
+                        <p>Evaluation and scope</p>
                     </div>
-                    <div className=''>
-                        <p>Icon</p>
-                        <p>Lorem ipsum dolor sit amet.</p>
+                    <div className='w-full bg-green-100 p-8 text-center rounded-xl shadow-2xl'>
+                        <img className='w-24 mx-auto' src={DevTesting}/>
+                        <p>Development and testing</p>
                     </div>
-                    <div className=''>
-                        <p>Icon</p>
-                        <p>Lorem ipsum dolor sit amet.</p>
+                    <div className='w-full bg-green-100 p-8 text-center rounded-xl shadow-2xl'>
+                        <img className='w-24 mx-auto' src={LaunchSupport}/>
+                        <p>Post-launch support</p>
                     </div>
                 </div>
             </div>            
