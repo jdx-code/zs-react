5072f18bee45daf3454f52c9719eb30a51bcd7b5
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-11-26 12:12:22 +0530

    cards given a grid layout for Offers section

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -2,39 +2,53 @@ import React from "react";
 
 const Offers = () => {
     return (
-        <div className="flex flex-col justify-center items-center bg-white text-gray-800 pt-24">
-            <h1 className="text-2xl text-center font-bold md:text-3xl">What we offer ?</h1>
-            <div className="w-full max-w-[1200px] flex flex-col justify-center items-center m-8 md:flex-row md:flex-wrap">
-                <div className="w-[280px] h-[300px] border-2 border-gray-700 rounded-lg m-12 px-12 py-8 flex flex-col justify-center items-center">
-                    <h1 className="text-2xl">Mobile Development</h1>                    
-                    <p>Tailor-made Mobile Applications</p>
-                    <p>Boost your business to the next level with our mobile development team.</p>
-                </div>
+        <div className="w-full bg-white text-black border-2 border-blue-500 py-12">
+            <div className='flex flex-col justify-center items-center px-4 md:px-24 text-gray-800'>
 
-                <div className="w-[280px] h-[300px] border-2 border-gray-700 rounded-lg m-12 px-12 py-8 flex flex-col justify-center items-center">
-                    <h1 className="text-2xl">Web Development</h1>                    
-                    <p>Scalable, efficient web apps</p>
-                    <p>Our web applications are reliable, efficient and regardless of the budget - made for easy scaling.</p>
-                </div>
+                <div className="mx-2 px-2 text-center md:mx-16 md:px-24">
+                    <h1 className="text-lg m-1 px-6 py-1 text-center font-bold md:m-6 md:text-3xl">What we offer ?</h1>
+                </div>            
+                
+                <div className="w-full grid md:grid-cols-6 justify-items-center gap-8 mt-8 mb-8">                
+                    <div className="w-full max-w-[85%] border-2 border-gray-700 rounded-lg p-12 text-center shadow-xl md:max-w-[99%] md:col-span-2">
+                        <h1 className="text-2xl">Mobile Development</h1>                    
+                        <p>Tailor-made Mobile Applications</p>
+                        <p>Boost your business to the next level with our mobile development team.</p>
+                    </div>
 
-                <div className="w-[280px] h-[300px] border-2 border-gray-700 rounded-lg m-12 px-12 py-8 flex flex-col justify-center items-center">
-                    <h1 className="text-2xl">Product Engineering</h1>                    
-                    <p>They will love your products.</p>
-                    <p>We use the best practices in UI as well as UX of digital products which lead to beautiful and intuitive interfaces.</p>
-                </div>
+                    <div className="w-full max-w-[85%] border-2 border-gray-700 rounded-lg p-12 text-center shadow-xl md:max-w-[99%] md:col-span-2">
+                        <h1 className="text-2xl">Web Development</h1>                    
+                        <p>Scalable, efficient web apps</p>
+                        <p>Our web applications are reliable, efficient and regardless of the budget - made for easy scaling.</p>
+                    </div>
 
-                <div className="w-[280px] h-[300px] border-2 border-gray-700 rounded-lg m-12 px-12 py-8 flex flex-col justify-center items-center">
-                    <h1 className="text-2xl">IT Services</h1>                    
-                    <p>Experts ready to work.</p>
-                    <p>Try our Website/SaaS Hosting, Remote IT outsourcing and Maintenance Support.</p>
-                </div>
+                    <div className="w-full max-w-[85%] border-2 border-gray-700 rounded-lg p-12 text-center shadow-xl md:max-w-[99%] md:col-span-2">
+                        <h1 className="text-2xl">Product Engineering</h1>                    
+                        <p>They will love your products.</p>
+                        <p>We use the best practices in UI as well as UX of digital products which lead to beautiful and intuitive interfaces.</p>
+                    </div>
+                    
+                    <div>
+                        
+                    </div>
 
-                <div className="w-[280px] h-[300px] border-2 border-gray-700 rounded-lg m-12 px-12 py-8 flex flex-col justify-center items-center">
-                    <h1 className="text-2xl">Cloud Services</h1>                    
-                    <p>For us, sky is not the limit.</p>
-                    <p>Efficient and flexible cloud based solutions for your data driven applications.</p>
+                    <div className="w-full max-w-[85%] border-2 border-gray-700 rounded-lg p-12 text-center shadow-xl md:max-w-[99%] md:col-span-2">
+                        <h1 className="text-2xl">IT Services</h1>                    
+                        <p>Experts ready to work.</p>
+                        <p>Try our Website/SaaS Hosting, Remote IT outsourcing and Maintenance Support.</p>
+                    </div>
+
+                    <div className="w-full max-w-[85%] border-2 border-gray-700 rounded-lg p-12 text-center shadow-xl md:max-w-[99%] md:col-span-2">
+                        <h1 className="text-2xl">Cloud Services</h1>                    
+                        <p>For us, sky is not the limit.</p>
+                        <p>Efficient and flexible cloud based solutions for your data driven applications.</p>
+                    </div>
+
+                    <div>
+                        
+                    </div>
                 </div>
-            </div>
+            </div>            
         </div>
     )
 }
--- src/components/OurProcess.jsx
+++ src/components/OurProcess.jsx
@@ -7,25 +7,25 @@ import LaunchSupport from '../assets/support-svgrepo-com.svg';
 const OurProcess = () => {
     return(
         <div className="w-full bg-white text-black border-2 border-blue-500 py-12">
-            <div className='flex flex-col justify-center items-center px-24 text-gray-800'>
-                <div className='mx-16 px-24 text-center'>
-                    <h1 className='text-2xl m-8 text-center font-bold md:text-3xl'>We take full-cycle care of our clients</h1>
-                    <p>Clarity, transparency, and integrity are among the core values of our team at Catware. As such, we don’t just “work on projects”. We bring our experience and expertise to every stage of the development lifecycle. You will have a reliable IT partner with Catware.</p>
+            <div className='flex flex-col justify-center items-center px-4 md:px-24 text-gray-800'>
+                <div className='mx-2 px-2 text-center md:mx-16 md:px-24'>
+                    <h1 className='text-lg m-1 px-6 py-1 text-center font-bold md:m-6 md:text-3xl'>We take full-cycle care of our clients</h1>
+                    <p className='text-sm px-2 md:text-base md:m-6'>Clarity, transparency, and integrity are among the core values of our team at Catware. As such, we don’t just “work on projects”. We bring our experience and expertise to every stage of the development lifecycle. You will have a reliable IT partner with Catware.</p>
                 </div>                
-                <div className='grid md:grid-cols-4 w-full gap-8 mt-8 mb-8'>
-                    <div className='w-full bg-pink-200 p-8 text-center rounded-xl shadow-2xl'>
+                <div className='w-full grid md:grid-cols-4 justify-items-center gap-8 mt-8 mb-8'>
+                    <div className='w-full max-w-[85%] bg-pink-100 border-2 border-pink-200 p-12 text-center rounded-lg shadow-xl md:max-w-[99%]'>
                         <img className='w-24 mx-auto' src={FirstInteraction}/>
                         <p>First Interaction</p>
                     </div>                                       
-                    <div className='w-full bg-pink-200 p-8 text-center rounded-xl shadow-2xl'>
+                    <div className='w-full max-w-[85%] bg-pink-100 border-2 border-pink-200 p-12 text-center rounded-lg shadow-xl md:max-w-[99%]'>
                         <img className='w-24 mx-auto' src={EvaluationScope}/>
                         <p>Evaluation and scope</p>
                     </div>
-                    <div className='w-full bg-pink-200 p-8 text-center rounded-xl shadow-2xl'>
+                    <div className='w-full max-w-[85%] bg-pink-100 border-2 border-pink-200 p-12 text-center rounded-lg shadow-xl md:max-w-[99%]'>
                         <img className='w-24 mx-auto' src={DevTesting}/>
                         <p>Development and testing</p>
                     </div>
-                    <div className='w-full bg-pink-200 p-8 text-center rounded-xl shadow-2xl'>
+                    <div className='w-full max-w-[85%] bg-pink-100 border-2 border-pink-200 p-12 text-center rounded-lg shadow-xl md:max-w-[99%]'>
                         <img className='w-24 mx-auto' src={LaunchSupport}/>
                         <p>Post-launch support</p>
                     </div>
