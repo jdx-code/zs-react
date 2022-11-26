265c652528b37bcad0cba7781558c541d7295d43
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-11-26 21:45:46 +0530

    Fixed OfferCard responsiveness

--- src/components/ChooseUs.jsx
+++ src/components/ChooseUs.jsx
@@ -3,13 +3,13 @@ import ChooseUsImage from '../assets/choose.png'
 
 const ChooseUs = () => {
     return (
-        <div className="w-full bg-white py-16 px-4">
-            <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-                <img className="w-[500px] mx-auto my-4" src={ChooseUsImage} alt="/" />
+        <div className="w-full bg-white py-8 px-4">
+            <div className="max-w-[1240px] mx-auto grid text-[#2c0f37] md:grid-cols-2">
+                <img className="w-[300px] mx-auto my-4 sm:w-[400px] md:w-[500px]" src={ChooseUsImage} alt="/" />
                 <div className="flex flex-col justify-center items-center text-center md:items-start md:text-left">
                     <h1 className="text-lg font-bold py-2 sm:text-2xl md:text-4xl">Why Choose Us ?</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates totam reprehenderit ducimus aliquid harum ipsa quos molestiae, commodi quas! Quis, culpa nulla. Error, quasi expedita recusandae officia minus ea itaque.</p>
-                    <button className="bg-black text-white font-medium px-4 py-2 my-4 rounded-md hover:bg-[#242124]">View projects</button>
+                    <p className="text-sm py-2 sm:text-lg md:text-xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates totam reprehenderit ducimus aliquid harum ipsa quos molestiae, commodi quas! Quis, culpa nulla. Error, quasi expedita recusandae officia minus ea itaque.</p>                    
+                    <button className="w-[115px] bg-gray-100 text-[#2c0f37] border-2 border-[#2c0f37] font-medium text-sm px-2 py-3 mx-auto md:mx-0 my-4 rounded-md sm:w-[145px] md:w-[160px] sm:text-lg md:text-xl hover:bg-[#2c0f37] hover:text-gray-100">View projects</button>
                 </div>
             </div>            
         </div>
--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -8,14 +8,14 @@ import ITStuffSVG from '../assets/it-stuff.svg'
 
 const Offers = () => {
     return (
-        <div className="w-full bg-white text-black border-2 border-blue-500 py-12">
-            <div className='flex flex-col justify-center items-center px-4 md:px-24 text-gray-800'>
+        <div className="w-full bg-white text-[#2c0f37] border-2 border-blue-500 py-12">
+            <div className='flex flex-col justify-center items-center px-4 md:px-24'>
 
-                <div className="mx-2 px-2 text-center md:mx-16 md:px-24">
-                    <h1 className="text-lg m-1 px-6 py-1 text-center font-bold sm:text-2xl md:text-4xl md:m-6">What we offer ?</h1>
+                <div className="mx-auto px-2 text-center md:px-24">                
+                    <h1 className="text-lg m-1 px-6 py-1 text-center font-bold sm:text-2xl md:text-3xl md:m-6">What we offer ?</h1>
                 </div>            
                 
-                <div className="w-full grid md:grid-cols-6 justify-items-center gap-8 mt-8 mb-8">                
+                <div className="w-full grid lg:grid-cols-6 justify-items-center gap-8 mt-8 mb-8">                
                     <OfferCard 
                         src={MobileSVG}
                         heading="Mobile Development" 
