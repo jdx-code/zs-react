823d6908a57d8f27dcf9c1e5937b158bc989d358
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-12-08 21:47:22 +0530

    Working on career component

--- src/Pages/Career.jsx
+++ src/Pages/Career.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import ChooseUsImage from '../assets/choose.png'
+import LaunchSupport from '../assets/support-svgrepo-com.svg';
+import Techstacks from '../components/Techstacks';
 
 const Career = () => {
     return(
-        <div>
-            <h1>Career</h1>
+<div>
+            <div className='careerClass w-full h-[500px]'> 
+                <div className='px-12 py-[245px] flex flex-col flex-wrap justify-center text-gray-300 md:w-[525px]'>
+                    <h1 className='text-4xl font-black py-2 md:text-5xl'>Join Our Team</h1>
+                    <p className='w-[300px]  text-md font-black md:text-lg md:w-full'>Collaborative working environment enabling you to learn and grow in quick time.</p>
+                </div>                
+            </div>
+            <div className='flex flex-col-reverse md:flex-row my-12'>
+                <div className='px-12 py-4 md:w-[50%]'>
+                    <h1 className='text-[1.8rem] font-black sm:text-[2rem] md:text-[2rem]'>Job Openings</h1>
+                    <p className='text-sm py-2 sm:text-lg md:text-xl'> No positions are open at this moment.</p>
+                    <p className='text-sm py-2 sm:text-lg md:text-lg'> You can still send us your updated CV </p>
+                    <p className='text-sm py-2 sm:text-lg md:text-lg font-bold italic underline'> career@zephyrsolutionsgroup.com </p>
+                </div>
+                <div className='px-12 py-4 md:w-[50%]'>
+                    <h1 className='text-[1.8rem] font-black sm:text-[2rem] md:text-[2rem]'>Internship Openings</h1>
+                    <p className='text-sm py-2 sm:text-lg md:text-xl'> Everyone needs to start somewhere. But with Zephyr, it's not any ordinary start. We offer industry based projects for Interns to prepare themselves for a better tomorrow.</p>                    
+                    <p className='text-sm py-2 sm:text-lg md:text-lg'> Register your name for an Internship Opportunity. Click <span className='text-xl font-bold text-purple-800'>here</span>.</p>
+                </div>
+            </div>                      
+            
+            <div className='careerStack'>
+                <Techstacks/>
+            </div>
+            
+            
         </div>
     )
 }
