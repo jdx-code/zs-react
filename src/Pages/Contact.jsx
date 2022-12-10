40c9ed5a814215324a7597874619133f51d80dfa
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-12-10 15:40:59 +0530

    Contact Form added

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Contact = () => {
     return(
-        <div>
-            <h1>Contact</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A laborum voluptatibus quo nihil placeat tempora consectetur non eaque aliquid illo.</p>
+        <div className='contactClass w-full p-8 grid grid-cols-2 justify-items-center'>                         
+            <div className='w-full max-w-[600px] p-2'>
+                <h1 className='text-xl text-blue-900 pb-4'>Write to us</h1>                        
+                <form method="post" action="" className='flex flex-col w-full max-w-[600px]'>
+                    <input className='p-4 rounded-lg border-2 border-purple-300 bg-purple-200' type="text" name="" placeholder="Your name"/>
+                    <input className='my-2 p-4 rounded-lg border-2 border-purple-300 bg-purple-200' type="email" name="" placeholder="Your email"/>
+                    <textarea className='p-4 rounded-lg border-2 border-purple-300 bg-purple-200' rows="10" placeholder="Your message"></textarea>
+                </form>
+            </div>            
+            <div className='w-full max-w-[600px] p-2 bg-[#06283D] rounded-lg'>
+                <div className='w-full p-24 text-white'>
+                    <p className='text-xl text-blue-100 pb-4'>Hello</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ullam voluptas magnam deleniti maiores recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sapiente alias porro laborum ad molestiae doloribus fugit commodi dolorum provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, ratione.</p>               
+                </div>                
+            </div>
         </div>
     )
 }
