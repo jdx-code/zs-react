d4827be89084a27dffd2496d376599ad445add4a
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-12-17 13:07:11 +0530

    Fixed contact component

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
     return(
         <div className='contactClass w-full p-8 grid md:grid-cols-2 justify-items-center'>                         
             <div className='w-full max-w-[600px] md:p-2'>
-                <h1 className='text-xl text-blue-900 pb-4'>Write to us</h1>                        
+                <h1 className='text-xl text-[#06283d] font-bold pb-4'>Write to us</h1>                        
                 <form method="post" action="" className='flex flex-col w-full'>
                     <input className='p-4 rounded-lg border-2 border-purple-300 bg-purple-200' type="text" name="" placeholder="Your name"/>
                     <input className='my-2 p-4 rounded-lg border-2 border-purple-300 bg-purple-200' type="email" name="" placeholder="Your email"/>
@@ -15,9 +15,16 @@ const Contact = () => {
             <div className='w-full max-w-[600px] mt-4 bg-[#06283D] rounded-lg md:mt-2 md:p-2'>
                 <div className='w-full px-6 py-16 md:py-24 lg:px-12 text-white'>                    
                     <blockquote>                    
-                        <p className='text-sm text-blue-100 lg:text-xl md:pb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ullam voluptas magnam deleniti maiores recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sapiente alias porro laborum ad molestiae doloribus fugit commodi dolorum provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, ratione.</p>               
-                        <span className='text-sm text-blue-100 lg:text-xl md:pb-4'>Web Studio</span>                        
+                        <p className='text-sm text-blue-100 lg:text-xl md:pb-4'>
+                        With our deep expertise in Customer and User Experience, we never lose sight of the ultimate goal. At Zephyr, we are dedicated to the success of your business by leveraging technology potential. Our team of engineers develops cutting-edge solutions for startups and established businesses.
+                        </p>               
+                        <span className='text-sm text-blue-100 lg:text-xl md:pb-4'>Zephyr Solutions</span>                        
                     </blockquote>
+                </div>
+                <div className='w-full px-6 lg:px-12 py-4 text-white'>
+                    <p className='text-xs md:text-lg leading-8'>Tell us your requirements</p>
+                    <p className='text-xs md:text-lg font-semibold leading-6'>Mail us <span className='underline italic'>info@zephyrsolutionsgroup.com</span></p>
+                    <p className='text-xs md:text-lg font-semibold'>Or call +91-7578094093</p>
                 </div>                  
             </div>
         </div>
